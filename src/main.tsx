e5953bd2c5f9a3aa346e4b73e8381a6f0b14c92a
BaoHaoYu <a450215151@outlook.com>
2019-10-28 10:27:14 +0800

    m: 使用函数式组件

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
 
-class Main extends React.Component<any> {
-  public render() {
-    return <h2>Hello World</h2>
-  }
+function Main() {
+  return <h2>Hello World</h2>
 }
 
 export default hot(module)(Main)
