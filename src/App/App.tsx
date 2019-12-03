2d607b3307014361c8ca6bfebd734ba509187040
BaoHaoYu <a450215151@outlook.com>
2019-12-03 09:11:26 +0800

    add: declaration.d.ts

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
-// @ts-ignore
 import logo from '../logo.svg'
-// @ts-ignore
 import css from './App.scss'
 
 function Main() {
