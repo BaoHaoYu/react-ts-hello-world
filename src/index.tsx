4c13a77f32e6f2937b75f0ae1bc36122fadc66cd
BaoHaoYu <a450215151@outlook.com>
2020-06-05 13:57:36 +0800

    add: index.scss, 全局的css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App/App'
+import './index.scss'
 
 function renderApp() {
   ReactDOM.render(<App />, document.querySelector('#root'))
