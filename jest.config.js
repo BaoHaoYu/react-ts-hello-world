1c6f2e8d91ab198ee1b51a91f5e56dba913a0994
BaoHaoYu <a450215151@outlook.com>
2020-06-28 09:59:08 +0800

    m: 使用jest配置可用

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-export default {
+module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   transform: {},
+  preset: 'ts-jest',
   moduleNameMapper: {
     '\\.(css|scss|less)$': 'identity-obj-proxy',
   },
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './style.scss'
 
 export function Demo(p: { msg: string }) {
   return (
     <div className={'Demo'}>
       <p className={'Demo__title'}>demo</p>
-      <span>{p.msg}</span>
+      <span className={'Demo__msg'}>{p.msg}</span>
     </div>
   )
 }
